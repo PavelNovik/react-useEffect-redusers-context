52cf6779d026b90b51ce24cba4b86dcab726ca19
PavelNovik <92117738+PavelNovik@users.noreply.github.com>
2023-05-12 19:12:36 +0300

    use hook useContext

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,52 +1,73 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 import styles from './Navigation.module.css';
 
 const Navigation = (props) => {
+  const context = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        <nav className={styles.nav}>
-          <ul>
-            {context.isLoggedIn && (
-              <li>
-                <a href="/">Пользователи</a>
-              </li>
-            )}
-            {context.isLoggedIn && (
-              <li>
-                <a href="/">Админ</a>
-              </li>
-            )}
-            {context.isLoggedIn && (
-              <li>
-                <button onClick={props.onLogout}>Выйти</button>
-              </li>
-            )}
-          </ul>
-        </nav>;
-      }}
-      {/* <nav className={styles.nav}>
-        <ul>
-          {props.isLoggedIn && (
-            <li>
-              <a href="/">Пользователи</a>
-            </li>
-          )}
-          {props.isLoggedIn && (
-            <li>
-              <a href="/">Админ</a>
-            </li>
-          )}
-          {props.isLoggedIn && (
-            <li>
-              <button onClick={props.onLogout}>Выйти</button>
-            </li>
-          )}
-        </ul>
-      </nav> */}
-    </AuthContext.Consumer>
+    // <AuthContext.Consumer>
+    //   {(context) => {
+    //     <nav className={styles.nav}>
+    //       <ul>
+    //         {context.isLoggedIn && (
+    //           <li>
+    //             <a href="/">Пользователи</a>
+    //           </li>
+    //         )}
+    //         {context.isLoggedIn && (
+    //           <li>
+    //             <a href="/">Админ</a>
+    //           </li>
+    //         )}
+    //         {context.isLoggedIn && (
+    //           <li>
+    //             <button onClick={props.onLogout}>Выйти</button>
+    //           </li>
+    //         )}
+    //       </ul>
+    //     </nav>
+    //   }}
+    //   {/* <nav className={styles.nav}>
+    //     <ul>
+    //       {props.isLoggedIn && (
+    //         <li>
+    //           <a href="/">Пользователи</a>
+    //         </li>
+    //       )}
+    //       {props.isLoggedIn && (
+    //         <li>
+    //           <a href="/">Админ</a>
+    //         </li>
+    //       )}
+    //       {props.isLoggedIn && (
+    //         <li>
+    //           <button onClick={props.onLogout}>Выйти</button>
+    //         </li>
+    //       )}
+    //     </ul>
+    //   </nav> */}
+    // </AuthContext.Consumer>
+
+    <nav className={styles.nav}>
+      <ul>
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Пользователи</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Админ</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Выйти</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
